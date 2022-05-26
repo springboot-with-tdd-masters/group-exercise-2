2152fee9a55dcd56f9edaaaaf1a33acae7b9405f
zaldydee <rizaldy.decena@softvision.com>
2022-05-26 18:24:59 +0800

    Added security and oauth

--- src/main/java/com/masters/masters/exercise/ExerciseApplication.java
+++ src/main/java/com/masters/masters/exercise/ExerciseApplication.java
@@ -5,10 +5,12 @@ import java.time.Clock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = "com.masters.masters.exercise.ExerciseApplication")
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/masters/masters/exercise/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.masters.masters.exercise.repository;
+
+import com.masters.masters.exercise.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+    User findByUsername(String username);
+}
