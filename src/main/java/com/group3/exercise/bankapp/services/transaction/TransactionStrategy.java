877e4c6fcba5ab8dbf35f26814574368f57d4081
marionino.obog <marioo@magenic.com>
2022-05-31 16:52:05 +0800

    [fix] use default method

--- src/main/java/com/group3/exercise/bankapp/services/transaction/TransactionStrategy.java
+++ src/main/java/com/group3/exercise/bankapp/services/transaction/TransactionStrategy.java
@@ -6,4 +6,11 @@ public interface TransactionStrategy<T extends Account> {
     T generateNewAccountDetails(String name, String acctNumber);
     T withdraw(T account, Double amount);
     T deposit(T account, Double amount);
+
+    default Double getDefaultValue(Double value){
+        if(value == null || value.isNaN()){
+            return 0.0;
+        }
+        return value;
+    }
 }
