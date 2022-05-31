bc1bad4bb68c4da86b9460b699d4819438f72efd
James Rod Mantaring <jamesrod.mantaring@ph.softvision.global>
2022-05-31 11:08:18 +0800

    [impl] extended base entity to transaction log

--- src/main/java/com/group3/exercise/bankapp/entities/TransactionLog.java
+++ src/main/java/com/group3/exercise/bankapp/entities/TransactionLog.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-public class TransactionLog {
+public class TransactionLog extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
